d55d1dead4463d1ac398c6682df97cfcba75e352
GeorgeZhang184 <89557663+GeorgeZhang184@users.noreply.github.com>
2023-07-20 12:49:35 +0800

    Create Focus-plus.py

---
+++ Focus-plus.py
@@ -0,0 +1,20 @@
+import time
+print("  ______                                  _______   _                           ")
+print(" |  ____|                                |__   __| (_)                          ")
+print(" | |__      ___     ___   _   _   ___       | |     _   _ __ ___     ___   _ __ ")
+print(" |  __|    / _ \   / __| | | | | / __|      | |    | | | '_ ` _ \   / _ \ | '__|")
+print(" | |      | (_) | | (__  | |_| | \__ \      | |    | | | | | | | | |  __/ | |   ")
+print(" |_|       \___/   \___|  \__,_| |___/      |_|    |_| |_| |_| |_|  \___| |_|   ")
+fotime=int(input("Focus Time (in minutes) :"))
+fosec=fotime*60
+flag=1
+while True:
+  minleft=fotime
+  secleft=fosec
+  divd=divmod(minleft,60)
+  print(divd[0],":",divd[1])
+  if divd[0]==0 and divd[1]==0:
+    print
+
+                                                                                
+                                                                                
